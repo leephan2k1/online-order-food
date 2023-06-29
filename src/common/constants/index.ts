7980158ef0f3b3313d03a0d6b6c30c091e6307ba
leephan2k1 <pttl2k1@gmail.com>
2023-06-29 14:27:45 +0700

    feat: 🎸 handle fetch categories

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -44,39 +44,6 @@ export const themeColors = {
   ...pallets[1],
 };
 
-export const categories = [
-  {
-    id: 1,
-    name: 'Pizza',
-    image: require('../../../assets/images/pizzaIcon.png'),
-  },
-  {
-    id: 2,
-    name: 'Burger',
-    image: require('../../../assets/images/pizzaIcon.png'),
-  },
-  {
-    id: 3,
-    name: 'Italian',
-    image: require('../../../assets/images/pizzaIcon.png'),
-  },
-  {
-    id: 4,
-    name: 'Chinese',
-    image: require('../../../assets/images/pizzaIcon.png'),
-  },
-  {
-    id: 5,
-    name: 'Noodles',
-    image: require('../../../assets/images/pizzaIcon.png'),
-  },
-  {
-    id: 6,
-    name: 'Sweets',
-    image: require('../../../assets/images/pizzaIcon.png'),
-  },
-];
-
 export const featured: Featured = {
   id: 912321,
   title: 'Hot and Spicy',
--- src/components/partials/Categories.tsx
+++ src/components/partials/Categories.tsx
@@ -1,9 +1,25 @@
 import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
-import { categories } from '~/common/constants';
-import { useState } from 'react';
+
+import { useEffect, useState } from 'react';
+import { getCategories } from '~/services/category.service';
+import { Category } from '~/common/interfaces/category.interface';
+import { urlFor } from '../../../Sanity';
 
 export default function Categories() {
-  const [activeCategory, setActiveCategory] = useState<number | null>(null);
+  const [activeCategory, setActiveCategory] = useState<string | null>(null);
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    (async function () {
+      try {
+        const data = await getCategories();
+
+        setCategories(data);
+      } catch (err) {
+        console.log('error: ', err);
+      }
+    })();
+  }, []);
 
   return (
     <View className='mt-2'>
@@ -14,22 +30,26 @@ export default function Categories() {
         contentContainerStyle={{ paddingHorizontal: 15 }}
       >
         {categories.map((item, index) => {
-          const isActive = activeCategory === item.id;
+          const isActive = activeCategory === item._id;
           const btnClass = isActive ? 'bg-gray-600' : 'bg-gray-200';
           const textClass = isActive ? 'text-gray-800' : 'text-gray-500';
 
           return (
             <View
-              key={item.id}
+              key={item._id}
               className={`flex items-center justify-center ${
                 index !== categories.length - 1 && 'mr-6'
               }`}
             >
               <TouchableOpacity
-                onPress={() => setActiveCategory(item.id)}
+                onPress={() => setActiveCategory(item._id)}
                 className={`p-2 rounded-full ${btnClass}`}
               >
-                <Image source={item.image} style={{ width: 45, height: 45 }} />
+                <Image
+                  className='rounded-full'
+                  source={{ uri: urlFor(item.image).url() }}
+                  style={{ width: 45, height: 45 }}
+                />
               </TouchableOpacity>
               <Text className={`${textClass} text-sm`}>{item.name}</Text>
             </View>
